d63eb7cb7d2dac452ab57a73b0396bcb7a5bef4a
Vlad <skyline_blade@mail.ru>
2023-10-27 09:51:07 +0300

    implemet functional of save todolist to LocalStorage

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import './App.css';
-import React,{useState} from 'react';
+import React,{useState,useEffect} from 'react';
 import TodoInputForm from './components/TodoInputForm';
 import TodoList from './components/TodoList';
 import Header from './components/Header';
 
 function App() {
-  const[todolist, setTodolist]=useState([]);
 
+  const data = JSON.parse(localStorage.getItem('todolist'));
+  // const data =null;
+  const[todolist, setTodolist]=useState( data ? data : []);
+
+  useEffect(()=>{localStorage.setItem('todolist', JSON.stringify(todolist));},[todolist])
 
   function Add(todo){
     setTodolist([...todolist,todo]);
-    // console.log(todolist);
   }
 
   function Delete(id){
